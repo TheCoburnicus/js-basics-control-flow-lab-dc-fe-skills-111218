9966f0ddf03fef35e6230a8d46a42cbbfc852de2
Nathan Coburn <nathanlcoburn@gmail.com>
2019-01-09 22:54:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someNumber) {
+  let result;
+  if (someNumber > 2500) {
+    result= 'No can do.';
+  return result;
+}
+  if (someNumber > 2000) {
+    result= 'I will gladly take your thirty bucks.';
+  return result;
+}
+if (someNumber <= 400) {
+    result= 'This one is on me!';
+  return result;
+}
+}
+
+function ternaryCheckCity(city) {
+ let result; 
+if (city === "NYC") {
+  ternaryCheckCity = true;
+  result= 'Ok, sounds good.';
+  return result;
+}
+else if (city !== "NYC") {
+  city = true;
+  result= 'No go.';
+  return result;
+}
+}
+
